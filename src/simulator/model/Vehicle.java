e0e0b5cddf3ae33c54c2dce89d3a8b623f5bff3e
Yello-Platipus <danieltinez1@gmail.com>
2022-02-02 12:47:30 +0100

    A

--- src/simulator/model/Vehicle.java
+++ src/simulator/model/Vehicle.java
@@ -2,9 +2,29 @@ package simulator.model;
 
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Vehicle extends SimulatedObject{
-    Vehicle(String id) {
+
+    private int maxSpeed;
+    private int currentSpeed;
+    private int location;
+    private int pollutionGroup;
+    private int totalPolluted;
+    private int distance;
+    private List<Junction> itinerary;
+    private VehicleStatus status;
+    private Road currentRoad;
+
+    Vehicle(String id, int maxSpeed, int contClass, List<Junction> itinerary) {
         super(id);
+        if(maxSpeed > 0 && contClass >= 0 && contClass <= 10 && itinerary.size() >= 2){
+            this.maxSpeed = maxSpeed;
+            pollutionGroup = contClass;
+            this.itinerary = Collections.unmodifiableList(new ArrayList<>(itinerary));
+        }
     }
 
     @Override
