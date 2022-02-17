9d81eea9b7da3a366e82c29a2fec3945b37238e6
Yello-Platipus <danieltinez1@gmail.com>
2022-02-17 12:00:40 +0100

    cositas hechas en las estrategias de mover cosas y ademas cositas de las juncciones y muchas mas cositas, por cierto, he añadido alguna cosa en el coso y mas cosas por las cosas

--- src/simulator/model/NewJunctionEvent.java
+++ src/simulator/model/NewJunctionEvent.java
@@ -1,12 +1,22 @@
 package simulator.model;
 
 public class NewJunctionEvent extends Event{
+    private String id;
+    private LightSwitchingStrategy lss;
+    private DequeuingStrategy dqs;
+    private int x, y;
     public NewJunctionEvent(int time, String id, LightSwitchingStrategy lsStrategy, DequeuingStrategy dqStrategy, int xCoor, int yCoor) {
         super(time);
+        this.id = id;
+        lss = lsStrategy;
+        dqs = dqStrategy;
+        x = xCoor;
+        y = yCoor;
     }
 
     @Override
     void execute(RoadMap map) {
-
+        Junction j = new Junction(id, lss, dqs, x, y);
+        map.addJunction(j);
     }
 }
---
+++ src/simulator/model/NewRoadEvent.java
@@ -0,0 +1,12 @@
+package simulator.model;
+
+public class NewRoadEvent extends Event{
+    NewRoadEvent(int time) {
+        super(time);
+    }
+
+    @Override
+    void execute(RoadMap map) {
+
+    }
+}
