e7ba23aad4ccd51c217c919433af7e331b3eb995
RoxyHentai <alexvallejos5b@gmail.com>
2022-02-19 14:21:33 +0100

    Vehiculos, carreteras basicas, carreteras interurbanas y urbanas medio hechas -Constructor coches mejorado -Constructor carretera mejorado -Report RoadMap (porfa funciona) -Eventos de las Carreteras (no tengo ni idea de que hice) -Eventos de los coches (no tengo ni idea de que hice (porfa funciona)) -setWeather inventado

--- src/simulator/model/SetWeatherEvent.java
+++ src/simulator/model/SetWeatherEvent.java
@@ -4,12 +4,26 @@ import simulator.misc.Pair;
 import java.util.List;
 
 public class SetWeatherEvent extends Event{
+    List<Pair<String,Weather>> ws;
     public SetWeatherEvent(int time, List<Pair<String,Weather>> ws) {
         super(time);
+        if(ws == null){
+            throw new IllegalArgumentException("The weather list in SetWeatherEvent is null");
+        }
+        this.ws = ws;
     }
 
     @Override
     void execute(RoadMap map) {
+        for(int i = 0; i < ws.size();i++){
+            try{
+                map.getRoad(ws.get(i).getFirst()).setWeather(ws.get(i).getSecond());
+            }
+            catch (Exception e){
+                throw new IllegalArgumentException("The road does not exists (SetWeatherEvent");
+            }
 
+
+        }
     }
 }
