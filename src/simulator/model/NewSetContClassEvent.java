4d94a7f0db3976d3b9110e2113d086715d3e2111
RoxyHentai <alexvallejos5b@gmail.com>
2022-02-19 14:24:42 +0100

    Vehiculos, carreteras basicas, carreteras interurbanas y urbanas medio hechas -Constructor coches mejorado -Constructor carretera mejorado -Report RoadMap (porfa funciona) -Eventos de las Carreteras (no tengo ni idea de que hice) -Eventos de los coches (no tengo ni idea de que hice (porfa funciona)) -setWeather inventado y set Contamination Class inventado

--- src/simulator/model/NewSetContClassEvent.java
+++ src/simulator/model/NewSetContClassEvent.java
@@ -4,12 +4,27 @@ import simulator.misc.Pair;
 import java.util.List;
 
 public class NewSetContClassEvent extends Event{
+    List<Pair<String,Integer>> cs;
     public NewSetContClassEvent(int time, List<Pair<String,Integer>> cs) {
         super(time);
+
+        if(cs == null){
+            throw new IllegalArgumentException("The contamination class in NewSetContClassEvent is null");
+        }
+        this.cs = cs;
     }
 
     @Override
     void execute(RoadMap map) {
+        for(int i = 0; i < cs.size();i++){
+            try{
+                map.getVehicle(cs.get(i).getFirst()).setContaminationClass(cs.get(i).getSecond());
+            }
+            catch (Exception e){
+                throw new IllegalArgumentException("The road does not exists (SetWeatherEvent");
+            }
 
+
+        }
     }
 }
